154157e03b5dc285400eb9845ae5dc82dc17cd4a
D4ve-Code <78406260+D4ve-Code@users.noreply.github.com>
2022-12-16 11:38:25 +0100

    hopeitsfinalcommit

--- src/screens/home.js
+++ src/screens/home.js
@@ -43,6 +43,7 @@ const handleClickNext = () => {
                 // style={{ width: "300px" }}
                 style={{ borderRadius: "1rem" }}
                 autoPlay
+                onLoadStart={0}
                 // layout="horizontal"
                 src={musicTracks[trackIndex].src}
                 onPlay={(e) => console.log("onPlay")}
@@ -55,143 +56,41 @@ const handleClickNext = () => {
                 />
                 </div>
             <Header />
-            {/* <img src={yoda} alt='yoda' className='yoda'/> */}
             <p>
-        <div class="a-long-time-ago">
-    A long time ago, in a galaxy far,<br /> far away..
-</div>
+                <div class="a-long-time-ago">
+                A long time ago, in a galaxy far,<br /> far away..
+                </div>
 
-<div class="crawl">
-    <div>
-        <p>
-            It is a period of civil war.
-            Rebel spaceships, striking
-            from a hidden base, have won
-            their first victory against
-            the evil Galactic Empire.
-        </p>
-        <p>
-            During the battle, Rebel
-            spies managed to steal secret
-            plans to the Empire's
-            ultimate weapon, the DEATH
-            STAR, an armored space
-            station with enough power to
-            destroy an entire planet.
-        </p>
-        <p>
-            Pursued by the Empire's
-            sinister agents, Princess
-            Leia races home aboard her
-            starship, custodian of the
-            stolen plan that can save
-            her people and restore
-            freedom to the galaxy....
+                <div class="crawl">
+                    <div>
+                        <p>
+                            It is a period of civil war.
+                            Rebel spaceships, striking
+                            from a hidden base, have won
+                            their first victory against
+                            the evil Galactic Empire.
+                        </p>
+                        <p>
+                            During the battle, Rebel
+                            spies managed to steal secret
+                            plans to the Empire's
+                            ultimate weapon, the DEATH
+                            STAR, an armored space
+                            station with enough power to
+                            destroy an entire planet.
+                        </p>
+                        <p>
+                            Pursued by the Empire's
+                            sinister agents, Princess
+                            Leia races home aboard her
+                            starship, custodian of the
+                            stolen plan that can save
+                            her people and restore
+                            freedom to the galaxy....
+                            </p>            
+                        </div>
+                        </div>
             </p>
-            <p>
-            It is a period of civil war.
-            Rebel spaceships, striking
-            from a hidden base, have won
-            their first victory against
-            the evil Galactic Empire.
-        </p>
-        <p>
-            During the battle, Rebel
-            spies managed to steal secret
-            plans to the Empire's
-            ultimate weapon, the DEATH
-            STAR, an armored space
-            station with enough power to
-            destroy an entire planet.
-        </p>
-        <p>
-            Pursued by the Empire's
-            sinister agents, Princess
-            Leia races home aboard her
-            starship, custodian of the
-            stolen plan that can save
-            her people and restore
-            freedom to the galaxy....
-            </p>
-            <p>
-            It is a period of civil war.
-            Rebel spaceships, striking
-            from a hidden base, have won
-            their first victory against
-            the evil Galactic Empire.
-        </p>
-        <p>
-            During the battle, Rebel
-            spies managed to steal secret
-            plans to the Empire's
-            ultimate weapon, the DEATH
-            STAR, an armored space
-            station with enough power to
-            destroy an entire planet.
-        </p>
-        <p>
-            Pursued by the Empire's
-            sinister agents, Princess
-            Leia races home aboard her
-            starship, custodian of the
-            stolen plan that can save
-            her people and restore
-            freedom to the galaxy....
-            </p>
-            <p>
-            It is a period of civil war.
-            Rebel spaceships, striking
-            from a hidden base, have won
-            their first victory against
-            the evil Galactic Empire.
-        </p>
-        <p>
-            During the battle, Rebel
-            spies managed to steal secret
-            plans to the Empire's
-            ultimate weapon, the DEATH
-            STAR, an armored space
-            station with enough power to
-            destroy an entire planet.
-        </p>
-        <p>
-            Pursued by the Empire's
-            sinister agents, Princess
-            Leia races home aboard her
-            starship, custodian of the
-            stolen plan that can save
-            her people and restore
-            freedom to the galaxy....
-            </p>
-            <p>
-            It is a period of civil war.
-            Rebel spaceships, striking
-            from a hidden base, have won
-            their first victory against
-            the evil Galactic Empire.
-        </p>
-        <p>
-            During the battle, Rebel
-            spies managed to steal secret
-            plans to the Empire's
-            ultimate weapon, the DEATH
-            STAR, an armored space
-            station with enough power to
-            destroy an entire planet.
-        </p>
-        <p>
-            Pursued by the Empire's
-            sinister agents, Princess
-            Leia races home aboard her
-            starship, custodian of the
-            stolen plan that can save
-            her people and restore
-            freedom to the galaxy....
-            </p>
-            
-        </div>
-        </div>
-        </p>
         </div>
     )
 }
